30df5ef0086a35b26030c3a26f673c19b7320b73
liuhang <617847070@qq.com>
2019-06-13 18:59:58 +0800

    1.添加butter knife 库

--- app/src/main/java/com/lh/kotlin/wanandroid/MainActivity.kt
+++ app/src/main/java/com/lh/kotlin/wanandroid/MainActivity.kt
@@ -2,11 +2,14 @@ package com.lh.kotlin.wanandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import butterknife.ButterKnife
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
+
     }
 }
